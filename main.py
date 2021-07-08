0a66bdb990e89c13073a7506704bfe72adfff6d0
Philip-Crowther <crowtherpb@gmail.com>
2021-07-08 18:54:10 -0400

    added some basic stuff

---
+++ main.py
@@ -0,0 +1,22 @@
+# this file makes compiles the poetry prompt input and makes calls to the open ai api
+import os, openai
+import pandas as pd
+import random as r
+
+NUMBER_OF_POEMS_CREATED = 1
+
+
+def generate_prompt():
+    """generates a selection of random poems as a prompt"""
+    prompt = ""
+    return prompt
+
+
+for _ in range(NUMBER_OF_POEMS_CREATED):
+    openai.api_key = os.getenv("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")
+    prompt = generate_prompt()
+    openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=5
+    )
