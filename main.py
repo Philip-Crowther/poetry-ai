9adb8a95dee22e39ec56b334e4d837a81172a015
Philip Crowther <crowtherpb@gmail.com>
2021-08-25 16:42:22 -0400

    corrected file description for its intended future use

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# this file makes compiles the poetry prompt input and makes calls to the open ai api
+# this file generates poems with the fine-tuned model
+
 import os, openai
 import random as r
 
 NUMBER_OF_POEMS_CREATED = 1
-
 
 for _ in range(NUMBER_OF_POEMS_CREATED):
     openai.api_key = os.getenv("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")
