e937603ad12b5477e5f381fd7e10a01a4967f104
Philip Crowther <crowtherpb@gmail.com>
2021-08-25 16:40:00 -0400

    removed prompt generation function

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ import os, openai
 import random as r
 
 NUMBER_OF_POEMS_CREATED = 1
-POEMS = open('poem_pieces.txt', 'r', encoding='utf-8').read().split('\n\n')
-PROMPT_LENGTH = 3000
-
-
-def generate_prompt():
-    """generates a selection of random poems as a prompt"""
-    api_prompt = ""
-    while len(api_prompt) < PROMPT_LENGTH-500:
-        api_prompt += r.choice(POEMS)
-        api_prompt += '\n'
-    return api_prompt
 
 
 for _ in range(NUMBER_OF_POEMS_CREATED):
