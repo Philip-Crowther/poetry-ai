fbc632b6a919e4500ae078ffcb96360f1a97ceff
Philip Crowther <crowtherpb@gmail.com>
2021-08-25 16:39:01 -0400

    described purpose of file

---
+++ generate_prompts.py
@@ -0,0 +1,4 @@
+# generates training prompts for the open-ai fine-tuning function. goal is to generate tweet-sized bits of poems,
+# then separate said poems to create training data of what we want the model to produce
+
+
