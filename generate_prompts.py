642ea431d59ffccf9e023ada572ec7fbe58a2a3f
Philip Crowther <crowtherpb@gmail.com>
2021-08-25 16:40:41 -0400

    placed old generate_prompt function in file

--- generate_prompts.py
+++ generate_prompts.py
@@ -1,4 +1,14 @@
 # generates training prompts for the open-ai fine-tuning function. goal is to generate tweet-sized bits of poems,
 # then separate said poems to create training data of what we want the model to produce
 
+POEMS = open('poem_pieces.txt', 'r', encoding='utf-8').read().split('\n\n')
+PROMPT_LENGTH = 3000
 
+
+def generate_prompt():
+    """generates a selection of random poems as a prompt"""
+    api_prompt = ""
+    while len(api_prompt) < PROMPT_LENGTH-500:
+        api_prompt += r.choice(POEMS)
+        api_prompt += '\n'
+    return api_prompt
