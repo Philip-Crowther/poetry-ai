6e357810360087989ea52866ac384599acf86a8f
Philip Crowther <crowtherpb@gmail.com>
2021-08-26 16:56:24 -0400

    trimmed size of poem pieces to reduce training costs

--- generate_prompts.py
+++ generate_prompts.py
@@ -1,14 +1,17 @@
 # generates training prompts for the open-ai fine-tuning function. goal is to generate tweet-sized bits of poems,
 # then separate said poems to create training data of what we want the model to produce
 
-POEMS = open('poem_pieces.txt', 'r', encoding='utf-8').read().split('\n\n')
-PROMPT_LENGTH = 3000
+MAX_PROMPT_LENGTH = 280  # based off length of tweets
 
 
-def generate_prompt():
-    """generates a selection of random poems as a prompt"""
-    api_prompt = ""
-    while len(api_prompt) < PROMPT_LENGTH-500:
-        api_prompt += r.choice(POEMS)
-        api_prompt += '\n'
-    return api_prompt
+def generate_prompts():
+    """creates twitter-sized training data"""
+    poem = ''
+    prompt, completion = cut_prompt(poem)
+    return
+
+
+def cut_prompt(poem):
+    """outputs semi-randomly sectioned poem as prompt and its ideal completion"""
+    return prompt, completion
+
