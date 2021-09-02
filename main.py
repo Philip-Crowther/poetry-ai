85362145f4d54f6bdd1a3defedb2be2a25f4c1c9
Philip Crowther <crowtherpb@gmail.com>
2021-09-02 12:38:35 -0400

    fix glaring security issue

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random as r
 NUMBER_OF_POEMS_CREATED = 1
 
 for _ in range(NUMBER_OF_POEMS_CREATED):
-    openai.api_key = os.getenv("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")
+    openai.api_key = os.getenv('OPEN_API_KEY')
     prompt = generate_prompt()
     openai.Completion.create(
         engine="davinci",
