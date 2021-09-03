0b053bdeee7562e012532daf847ba161a31227d1
Philip Crowther <crowtherpb@gmail.com>
2021-09-02 20:16:20 -0400

    create small-ish training set
    I now have a dataframe of a small selection of poets' work to test the fine-tuning openai function with. In order to use it, I will have to trim it down to just poems and save it as a csv file then use openai's tool to format the file. Then all that remains is to fine-tune.

--- create_test_training_files.py
+++ create_test_training_files.py
@@ -1,2 +1,11 @@
 # file will be used to create a smaller dataset of poems to test the GPT fine-tuning features
+import pandas as pd
 
+data = pd.read_csv('kaggle_poem_dataset.csv')
+
+
+poets = ['Emily Dickinson', 'Walt Whitman', 'William Wordsworth', 'Robert Frost', 'Maya Angelou', 'Sylvia Plath',
+         'Gwendolyn Brooks', 'Edgar Allan Poe', 'Langston Hughes', 'James Baldwin', 'Rita Dove',
+         'William Carlos Williams', 'Allen Ginsberg', 'Lucille Clifton', 'Audre Lorde']
+
+poems = data.loc[data['Author'].isin(poets)]
