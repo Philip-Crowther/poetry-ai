c7d74a6267a6f8c57a5d34a7ac7b42189b5e0f75
Philip Crowther <crowtherpb@gmail.com>
2021-10-26 20:28:29 -0400

    add messages to build_api func

--- twitter_bot/build_api.py
+++ twitter_bot/build_api.py
@@ -4,11 +4,13 @@ import os
 
 def build_api():
     # Authenticate to Twitter
+    print("authenticating")
     auth = tweepy.OAuthHandler(os.getenv("MUSE_AI_CONSUMER_KEY"), os.getenv("MUSE_AI_CONSUMER_SECRET"))
     auth.set_access_token(os.getenv("MUSE_AI_ACCESS_TOKEN"), os.getenv("MUSE_AI_ACCESS_TOKEN_SECRET"))
 
     # Create API object
     api = tweepy.API(auth)
+    print("api created")
     return api
 
 def main():
