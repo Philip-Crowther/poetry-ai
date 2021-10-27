79bd6c99fa0f91e33dc45facd25691e671b61c0d
Philip Crowther <crowtherpb@gmail.com>
2021-10-27 19:26:06 -0400

    fix some formatting issues

--- twitter_bot/build_api.py
+++ twitter_bot/build_api.py
@@ -1,12 +1,15 @@
 # build api function
-import tweepy
 import os
+import tweepy
 
 def build_api():
+    """reusable function to build api object"""
     # Authenticate to Twitter
     print("authenticating")
-    auth = tweepy.OAuthHandler(os.getenv("MUSE_AI_CONSUMER_KEY"), os.getenv("MUSE_AI_CONSUMER_SECRET"))
-    auth.set_access_token(os.getenv("MUSE_AI_ACCESS_TOKEN"), os.getenv("MUSE_AI_ACCESS_TOKEN_SECRET"))
+    auth = tweepy.OAuthHandler(os.getenv("MUSE_AI_CONSUMER_KEY"), \
+        os.getenv("MUSE_AI_CONSUMER_SECRET"))
+    auth.set_access_token(os.getenv("MUSE_AI_ACCESS_TOKEN"), \
+        os.getenv("MUSE_AI_ACCESS_TOKEN_SECRET"))
 
     # Create API object
     api = tweepy.API(auth)
@@ -14,6 +17,7 @@ def build_api():
     return api
 
 def main():
+    """main function"""
     pass
 
 if __name__ == "__main__":
--- twitter_bot/follow_back.py
+++ twitter_bot/follow_back.py
@@ -1,13 +1,15 @@
-# follow back all followers not previously followed
+#s follow back all followers not previously followed
 import tweepy
 import build_api
 
 def follow(api):
+    """follow unfollowed followers"""
     for follower in tweepy.Cursor(api.get_followers).items():
         if not follower.following:
             follower.follow()
 
 def main():
+    """build api and follow followers"""
     print("creating api")
     api = build_api.build_api()
     print("executing follow function")
