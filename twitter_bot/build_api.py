438515e676638b4a80df121282b645185602d6e3
Philip Crowther <crowtherpb@gmail.com>
2021-10-26 20:14:38 -0400

    complete build_api function

--- twitter_bot/build_api.py
+++ twitter_bot/build_api.py
@@ -1,17 +1,18 @@
-# twitter bot 
+# build api function
 import tweepy
 import os
-import time
 
-# Authenticate to Twitterp
-auth = tweepy.OAuthHandler(os.getenv("MUSE_AI_CONSUMER_KEY"), os.getenv("MUSE_AI_CONSUMER_SECRET"))
-auth.set_access_token(os.getenv("MUSE_AI_ACCESS_TOKEN"), os.getenv("MUSE_AI_ACCESS_TOKEN_SECRET"))
+def build_api():
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(os.getenv("MUSE_AI_CONSUMER_KEY"), os.getenv("MUSE_AI_CONSUMER_SECRET"))
+    auth.set_access_token(os.getenv("MUSE_AI_ACCESS_TOKEN"), os.getenv("MUSE_AI_ACCESS_TOKEN_SECRET"))
 
-# Create API object
-api = tweepy.API(auth)
+    # Create API object
+    api = tweepy.API(auth)
+    return api
 
-# follow all unfollowed followers
-for follower in tweepy.Cursor(api.get_followers).items():
-    if not follower.following:
-        follower.follow()
+def main():
+    pass
 
+if __name__ == "__main__":
+    main()
